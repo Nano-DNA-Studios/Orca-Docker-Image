72c1ba0e33e7bb4f19f88a4a79542b5147fa210b
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2024-07-26 01:12:42 -0400

    Added an Upload Script

---
+++ Upload.py
@@ -0,0 +1,9 @@
+import subprocess
+
+# Build Image
+subprocess.run(['docker', 'build', '-t', 'mrdnalex/orca', '.' ], check=True)
+
+# Upload Image
+#subprocess.run(['docker', 'login', '-u', 'mrdnalex', '&&', 'docker', 'push', 'mrdnalex/orca' ], check=True)
+
+# docker login -u mrdnalex && docker push mrdnalex/orca
